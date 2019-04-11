c5b675b77afa07599519748225099a227ed6dc44
badaei@yahoo.com <badaei@yahoo.com>
2019-04-11 15:32:05 -0700

    readme update

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,26 @@
 import { Reverse } from 'typescript-tuple';
+/**
+ * Extracts function arguments
+ */
 export declare type ExtractFunctionArguments<Fn> = Fn extends (...args: infer P) => any ? P : never;
+/**
+ * Extracts function return values
+ */
 export declare type ExtractFunctionReturnValue<Fn> = Fn extends (...args: any[]) => infer P ? P : never;
 declare type BooleanSwitch<Test, T = true, F = false> = Test extends true ? T : F;
-declare type Arbitrary = 'It is now 1554792354 since seconds since Jan 01, 1970';
+/**
+ * Replacement for Function, represents any kind of function.
+ */
 export declare type AnyFunction = (...args: any[]) => any;
-export declare type Function1 = (a: any) => unknown;
-export declare type IsAny<O, T = true, F = false> = Arbitrary extends O ? any extends O ? T : F : F;
+/**
+ * Represents any function with an arity of 1.
+ */
+export declare type AnyFunction1 = (a: any) => any;
+declare type Arbitrary = 'It is now 1554792354 since seconds since Jan 01, 1970';
+declare type IsAny<O, T = true, F = false> = Arbitrary extends O ? any extends O ? T : F : F;
+/**
+ * A powerful recursive type function composition using `pipe`.
+ */
 export declare type Pipe<Fns extends any[], IsPipe = true, PreviousFunction = void, InitalParams extends any[] = any[], ReturnType = any> = {
     'next': ((..._: Fns) => any) extends ((_: infer First, ..._1: infer Next) => any) ? PreviousFunction extends void ? Pipe<Next, IsPipe, First, ExtractFunctionArguments<First>, ExtractFunctionReturnValue<First>> : ReturnType extends ExtractFunctionArguments<First>[0] ? Pipe<Next, IsPipe, First, InitalParams, ExtractFunctionReturnValue<First>> : IsAny<ReturnType> extends true ? Pipe<Next, IsPipe, First, InitalParams, ExtractFunctionReturnValue<First>> : {
         ERROR: ['Return type ', ReturnType, 'does comply with the input of', ExtractFunctionArguments<First>[0]];
@@ -13,11 +28,23 @@ export declare type Pipe<Fns extends any[], IsPipe = true, PreviousFunction = vo
     } : never;
     'done': (...args: InitalParams) => ReturnType;
 }[Fns extends [] ? 'done' : 'next'];
+/**
+ * A powerful recursive type function composition using `compose`.
+ */
 export declare type Compose<Fns extends any[]> = Pipe<Reverse<Fns>, false>;
-export declare type PipeFn = <Fns extends [AnyFunction, ...Function1[]]>(...fns: Fns & Pipe<Fns> extends AnyFunction ? Fns : never) => Pipe<Fns>;
-export declare type PipelineFn = <Arg, Fns extends [(arg: Arg) => any, ...Function1[]]>(arg: Arg, ...fns: Fns & Pipe<Fns> extends AnyFunction ? Fns : never) => ExtractFunctionReturnValue<Pipe<Fns>> extends {
+/**
+ * An example of `Pipe` used in a `pipe` function
+ */
+export declare type PipeFn = <Fns extends [AnyFunction, ...AnyFunction1[]]>(...fns: Fns & Pipe<Fns> extends AnyFunction ? Fns : never) => Pipe<Fns>;
+/**
+ * An example of `Pipe` used in a `pipeline` function (a `pipe` that is immediately evaluated)
+ */
+export declare type PipelineFn = <Arg, Fns extends [(arg: Arg) => any, ...AnyFunction1[]]>(arg: Arg, ...fns: Fns & Pipe<Fns> extends AnyFunction ? Fns : never) => ExtractFunctionReturnValue<Pipe<Fns>> extends {
     ERROR: [string];
     POSITION: [string];
 } ? Pipe<Fns> : ExtractFunctionReturnValue<Pipe<Fns>>;
+/**
+ * An example of `Compose` used in a `compose` function
+ */
 export declare type ComposeFn = <Fns extends [AnyFunction, ...AnyFunction[]]>(...fns: Fns & Compose<Fns> extends AnyFunction ? Fns : never) => Compose<Fns>;
 export {};
--- readme.md
+++ readme.md
@@ -14,22 +14,25 @@ The main benefit of the overloading approach to this recursive type is that when
 # Exports
 
 
-## Main
+### Main
 Key exported types include 
 
 * `Pipe` as mentioned above
-* `Compose`
-* `PipeFn`
-* `ComposeFn`
-* `PipelineFn`
+* `Compose` data flows from bottom up ( or left to right )
+* `PipeFn`  Pipe applied to create a reference function type
+* `ComposeFn` Compose applied to create a reference function type
+* `PipelineFn` Pipe applied to create a useful `pipeline` function type
 
-## Secondary
+### Secondary
 
 Other types exported
-* ExtractFunctionArguments
-* ExtractFunctionReturnValue
-* AnyFunction
-* AnyFunction1
+* `ExtractFunctionArguments` extracts function arguments
+* `ExtractFunctionReturnValue` extracts a functions return type
+
+### Circumstantial 
+Ad-hoc types that may aid in constructing applications from these types.
+* `AnyFunction` a type representing any kind of function, an alternative to Function
+* `AnyFunction1` a function representing any function with an arity of 1
 
 # Installation
 
@@ -40,10 +43,13 @@ Other types exported
 Example:
 
 ```ts
-import { PipeFn } from 'pipe-and-compose-types`
+import { PipeFn } from 'pipe-and-compose-types'
 
+declare const example: ( first: number, rest: number[] ) => number
 declare const pipe: PipeFn
 
-const pipe(
-  
-)
+const result = pipe(
+  example,
+  String
+) // (first: number, rest: number[]) => string
+```
